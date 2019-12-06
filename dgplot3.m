68b3049ea45dfe2fbe3e99716af16bf9e466c78f
KathyW666 <13222024@bjtu.edu.cn>
2019-12-06 18:00:30 +0800

    implemented plot function for 3D digraph

---
+++ dgplot3.m
@@ -0,0 +1,22 @@
+function h = dgplot3(A, xyz, varargin)
+ % If no arguments given, then run buckminster sphere example
+    if nargin == 0
+        [A, xyz] = bucky;
+    end
+
+    % If only one argument given, throw error.
+    if nargin == 1
+        error('Please provide an adjacency matrix and coordinate array');
+    end
+    
+    % Returns i and j, lists of connected nodes
+    [s,d] = find(A);
+    
+    % Extact 
+    start = xyz(s,:);
+    desti = xyz(d,:);
+    
+    % If only two arguments, then plot as is
+    if nargin == 0 || nargin == 2
+        h = arrow3d(start,desti,10,'line',0.05,3);
+    end
